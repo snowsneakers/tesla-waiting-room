f3e6dfb696b3cee23c4a7aca6d7e541e6e8ac095
Patrick Snowden <patrickbsnowden@outlook.com>
2022-09-16 10:55:58 -0700

    changed user id to mongoose.Schema.Types

--- backend/models/commentModel.js
+++ backend/models/commentModel.js
@@ -5,9 +5,9 @@ const commentModel = new mongoose.Schema(
           text: { type: String, required: true },
           likes: { type: Array, requried: true, default: [] },
           liked: { type: Boolean, required: true, default: false },
-          user_id: { type: String, requried: true },
+          user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
           user_username: {type: String, required: true},
-          post_id: {type: String, required: true}
+          post_id: {type: mongoose.Schema.Types.ObjectId, ref: "Entry"}
      },
      { timestamps: true }
 );
